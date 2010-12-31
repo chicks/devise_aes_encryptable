58281dfc2dd28c2f0804800e8b16723ca6c20e3f
Carl Hicks <carl.hicks@gmail.com>
2010-12-30 16:45:12 -0800

    Added instructions on usage

--- lib/encryptors/aes256.rb
+++ lib/encryptors/aes256.rb
@@ -5,20 +5,20 @@ module Devise
     # = AES
     # Uses the AES algorithm to encrypt passwords.
     class Aes256 < Base
-      # Returns a Base64 encrypted password where pepper is used for the key,
-      # and the initialization_vector is randomly generated and prepended onto
-      # encoded ciphertext
-      def self.digest(password, stretches, salt, pepper)
-        digest = ::AES.encrypt(pepper, password)
-      end
       class << self
+        # Returns a Base64 encrypted password where pepper is used for the key,
+        # and the initialization_vector is randomly generated and prepended onto
+        # encoded ciphertext
+        def digest(password, stretches, salt, pepper)
+          digest = ::AES.encrypt(pepper, password)
+        end
         alias :encrypt :digest
-      end
-      
-      # Returns the plaintext password where pepper is used for the key,
-      # and the initialization_vector is read from the Base64 encoded ciphertext
-      def self.decrypt(encrypted_password, pepper)
-        password = ::AES.decrypt(pepper, encrypted_password)
+        
+        # Returns the plaintext password where pepper is used for the key,
+        # and the initialization_vector is read from the Base64 encoded ciphertext
+        def decrypt(encrypted_password, pepper)
+          password = ::AES.decrypt(pepper, encrypted_password)
+        end
       end
     end
   end
